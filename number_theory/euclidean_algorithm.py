446ce92395f61ae65ec98fd286b3190c57a7b06e
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-17 01:34:38 -0400

    update

---
+++ number_theory/euclidean_algorithm.py
@@ -0,0 +1,1 @@
+euclidian_algorithm
---
+++ number_theory/rational_numbers.py
@@ -0,0 +1,9 @@
+import math
+
+
+
+
+def find_period_decimal(numberator = 22, denominator = 3):
+    pass
+    # all rational number have a periodic decimal, so this function finds the length, starting index, and the repeating decimal values
+
